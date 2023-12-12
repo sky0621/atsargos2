d72ec61e621b549c4bdd0e7be0722de825537d1b
sky0621 <sky14work@gmail.com>
2023-12-12 21:46:45 +0900

    -

---
+++ frontend/src/pages/Login/Login.tsx
@@ -0,0 +1,10 @@
+
+const LoginComponent: React.FC = () => {
+    return (
+        <div>
+            <h2>Login Page</h2>
+            {/* Your login logic here */}
+        </div>
+    );
+};
+export default LoginComponent;
