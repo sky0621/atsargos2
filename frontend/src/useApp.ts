10d85eeeb16bf382f864b9075af791daf57a4cf8
sky0621 <sky14work@gmail.com>
2023-12-13 22:05:18 +0900

    -

--- frontend/src/useApp.ts
+++ frontend/src/useApp.ts
@@ -1,9 +1,24 @@
 import { useState } from "react";
+import { firebaseAuth, provider } from "./main.tsx";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useCookies } from "react-cookie";
+import { DateTime } from "luxon";
 
 export const useApp = () => {
   const [loggedIn, setLoggedIn] = useState<boolean>(false);
-
-  const login = () => {
+  const [_, setCookie] = useCookies(["atsargos2-id-token"]);
+  const login = async () => {
+    try {
+      const result = await signInWithPopup(firebaseAuth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      console.info(credential);
+      setCookie("atsargos2-id-token", credential?.idToken, {
+        expires: DateTime.now().plus({ days: 7 }).toJSDate(),
+      });
+    } catch (e) {
+      console.error(e);
+      return;
+    }
     setLoggedIn(true);
   };
 
